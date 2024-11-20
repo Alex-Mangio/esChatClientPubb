df4682974c17007e5fc5054793a9e9e53a609bc6
lai2-24 <lai2-24@itismeucci.com>
2024-11-20 10:03:44 +0100

    commit dylan

--- client_pubb/src/main/java/client_pubb/ThreadClient.java
+++ client_pubb/src/main/java/client_pubb/ThreadClient.java
@@ -27,119 +27,133 @@ public class ThreadClient extends Thread {
 
             System.out.println("Ti sei connesso con il server");
 
+            boolean connessione = true;
+
             // gestione username
-            String benvenuto = in.readLine();
-            System.out.println(benvenuto);
-            Scanner inputUsername = new Scanner(System.in);
-            out.writeBytes(inputUsername.nextLine() + "\n");
-
-            // MENU PRINCIPALE
-            String menu = in.readLine();
-
-            System.out.println("MENU PRINCIPALE \n");
-            System.out.println("1)Digita '1' per andare in chat PRIVATA \n");
-            System.out.println("2)Digita '2' per andare in chat PUBBLICA \n");
-            System.out.println("3)Digita '3' per disconnetterti \n");
-
-            Scanner sceltaMenuPrinc = new Scanner(System.in);
-
             do {
-                switch (sceltaMenuPrinc.nextLine()) {
+                String benvenuto = in.readLine();
+                System.out.println(benvenuto);
+                Scanner inputUsername = new Scanner(System.in);
+                out.writeBytes(inputUsername.nextLine() + "\n");
 
-                    case "1":
-                        out.writeBytes("PRIVATO" + "\n");
-                        String risposta1 = in.readLine();
+                // MENU PRINCIPALE
+                String menu = in.readLine();
 
-                        // MENU per le chat PRIVATE
-                        System.out.println("Scegli cosa vuoi fare in chat privata: \n");
-                        System.out.println("1)Digita '1' per vedere la lista delle tue chat PRIVATE \n");
-                        System.out.println("2)Digita '2' per iniziare a chattare con un tuo contatto \n");
-                        System.out.println("3)Digita '3' per tornare al MENU PRINCIPALE \n");
+                if (menu.equals("MENU")) {
 
-                        Scanner sceltaMenuPriv = new Scanner(System.in);
+                    System.out.println("MENU PRINCIPALE \n");
+                    System.out.println("1)Digita '1' per andare in chat PRIVATA \n");
+                    System.out.println("2)Digita '2' per andare in chat PUBBLICA \n");
+                    System.out.println("3)Digita '3' per disconnetterti \n");
 
-                        switch (sceltaMenuPriv.nextLine()) {
+                    Scanner sceltaMenuPrinc = new Scanner(System.in);
+
+                    do {
+                        switch (sceltaMenuPrinc.nextLine()) {
 
                             case "1":
-                                // stampa la lista dei contatti PRIVATI
-                                ObjectInputStream objIn = new ObjectInputStream(s.getInputStream());
-                                List contatti = new ArrayList<String>();
+                                out.writeBytes("PRIVATO" + "\n");
+                                String risposta1 = in.readLine();
 
-                                out.writeBytes("LISTA_CONTATTI" + "\n");
+                                // MENU per le chat PRIVATE
+                                System.out.println("Scegli cosa vuoi fare in chat privata: \n");
+                                System.out.println("1)Digita '1' per vedere la lista delle tue chat PRIVATE \n");
+                                System.out.println("2)Digita '2' per iniziare a chattare con un tuo contatto \n");
+                                System.out.println("3)Digita '3' per tornare al MENU PRINCIPALE \n");
 
-                                contatti = (List) objIn.readObject();
+                                Scanner sceltaMenuPriv = new Scanner(System.in);
 
-                                if (contatti.size() > 0) {
-                                    for (int i = 0; i < contatti.size(); i++) {
-                                        System.out.println(contatti.get(i));
-                                    }
-                                } else {
-                                    System.out.println("Non hai nessun contatto privato registrato \n");
+                                switch (sceltaMenuPriv.nextLine()) {
+
+                                    case "1":
+                                        // stampa la lista dei contatti PRIVATI
+                                        ObjectInputStream objIn = new ObjectInputStream(s.getInputStream());
+                                        ArrayList contatti = new ArrayList<String>();
+
+                                        out.writeBytes("LISTA_CONTATTI" + "\n");
+
+                                        contatti = (ArrayList) objIn.readObject();
+
+                                        if (contatti.size() > 0) {
+                                            for (int i = 0; i < contatti.size(); i++) {
+                                                System.out.println(contatti.get(i));
+                                            }
+                                        } else {
+                                            System.out.println("Non hai nessun contatto privato registrato \n");
+                                        }
+                                        break;
+
+                                    case "2":
+                                        break;
+
+                                    case "3":
+
+                                        break;
+
                                 }
+
                                 break;
 
                             case "2":
-                                break;
+                                out.writeBytes("PUBBLICO" + "\n");
+                                String risposta2 = in.readLine();
 
-                            case "3":
+                                do {
+                                    // MENU per le chat PUBBLICHE
+                                    System.out.println("Scegli cosa vuoi fare in chat pubblica: \n");
+                                    System.out.println("1)Digita '1' per vedere la lista dei tuoi gruppi PUBBLICI \n");
+                                    System.out.println("2)Digita '2' per iniziare a chattare con un gruppo \n");
+                                    System.out.println("3)Digita '3' per tornare al MENU PRINCIPALE \n");
 
-                                break;
+                                    Scanner sceltaMenuPubb = new Scanner(System.in);
 
-                        }
+                                    switch (sceltaMenuPubb.nextLine()) {
 
-                        break;
+                                        case "1":
+                                            // stampa la lista dei gruppi PUBBLICI
+                                            ArrayList<String> gruppi = new ArrayList<String>();
 
-                    case "2":
-                        out.writeBytes("PUBBLICO");
-                        String risposta2 = in.readLine();
+                                            out.writeBytes("LISTA_PUBBL" + "\n");
 
-                        // MENU per le chat PUBBLICHE
-                        System.out.println("Scegli cosa vuoi fare in chat pubblica: \n");
-                        System.out.println("1)Digita '1' per vedere la lista dei tuoi gruppi PUBBLICI \n");
-                        System.out.println("2)Digita '2' per iniziare a chattare con un gruppo \n");
-                        System.out.println("3)Digita '3' per tornare al MENU PRINCIPALE \n");
+                                            while (true) {
+                                                String lista = in.readLine();
+                                                if (lista.equals("VUOTO")) {
+                                                    System.out.println("Non hai gruppi registrati \n");
+                                                    break;
+                                                } else {
+                                                    gruppi.add(lista);
+                                                }
+                                            }
 
-                        Scanner sceltaMenuPubb = new Scanner(System.in);
+                                            if (!gruppi.isEmpty()) {
+                                                for (String i : gruppi) {
+                                                    System.out.println(i + "\n");
+                                                }
+                                            }
+                                            break;
 
-                        switch (sceltaMenuPubb.nextLine()) {
+                                        case "2":
+                                            break;
 
-                            case "1":
-                                // stampa la lista dei gruppi PUBBLICI
-                                ObjectInputStream objIn = new ObjectInputStream(s.getInputStream());
-                                List gruppi = new ArrayList<String>();
+                                        case "3":
 
-                                out.writeBytes("LISTA_GRUPPI" + "\n");
+                                            break;
 
-                                gruppi = (List) objIn.readObject();
-
-                                if (gruppi.size() > 0) {
-                                    for (int i = 0; i < gruppi.size(); i++) {
-                                        System.out.println(gruppi.get(i));
                                     }
-                                } else {
-                                    System.out.println("Non hai nessun gruppo pubblico registrato \n");
-                                }
-                                break;
-
-                            case "2":
-                                break;
+                                } while (true);
 
                             case "3":
-
+                                // DISCONNESSIONE client
+                                out.writeBytes("DISCONNETTI" + "\n");
+                                String risposta3 = in.readLine();
                                 break;
 
                         }
-                        break;
-
-                    case "3":
-                        // DISCONNESSIONE client
-                        out.writeBytes("DISCONNETTI");
-                        String risposta3 = in.readLine();
-                        break;
-
+                    } while (connessione);
+                } else {
+                    System.out.println(menu);
                 }
             } while (true);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
